e108928b7c4da352f1a434d6874b2a16e38d1472
Jhol <galilleu2011@gmail.com>
2022-10-22 10:20:41 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jhol Moreira on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
