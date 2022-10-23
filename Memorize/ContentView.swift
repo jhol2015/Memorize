b8af1d2d9573c3bfd195bf9cba8da14a1597208c
Jhol Moreira Mendanha <galilleu2011@gmail.com>
2022-10-23 20:32:42 -0300

    Ajustado cartas na tela, criado botões para adicionar e remover.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,16 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var emojis = ["🚢", "🛫", "🚆", "🚘", "🚨", "🦈", "🦖", "🤖", "🎃", "👽", "🛺", "🚔"]
-    var emojiCount = 6
+    @State var emojiCount = 6
     
     var body: some View {
-        HStack {
-            ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                CardView(content: emoji)
+        VStack {
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    //id: \.self = diz par ao forEach que o array tem um identificador e assim não repete as figuras.
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
             }
+            .foregroundColor(.red)
+            
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+        
+    }
+    
+    var add: some View {
+        Button{
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            VStack {
+                Image(systemName: "plus.circle")
+            }
+        }
+    }
+    var remove: some View {
+        Button{
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            VStack {
+                Image(systemName: "minus.circle")
+            }
+        }
     }
 }
 
@@ -31,7 +69,7 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             }else {
                 shape.fill()
