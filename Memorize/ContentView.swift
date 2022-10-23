597d026782bf45e061bf32bf44148f64f223d1ce
Jhol Moreira Mendanha <galilleu2011@gmail.com>
2022-10-22 23:14:17 -0300

    create random emojis.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,20 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚢", "🛫", "🚆", "🚘", "🚨", "🦈", "🦖", "🤖", "🎃", "👽", "🛺", "🚔"]
+    var emojiCount = 6
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Ola Mundo!")
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        HStack {
+            ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                CardView(content: emoji)
+            }
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack() {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
